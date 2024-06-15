d444be785ead852a3876e125ada52906e8596230
sabeeh21 <onamouceclick@gmail.com>
2024-06-15 15:56:51 +0500

    dgdfgfd

--- dist/index.5baa4167.js
+++ dist/index.5baa4167.js
@@ -41966,10 +41966,61 @@ function isAction(action) {
 }
 
 },{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"iYVDd":[function(require,module,exports) {
+// const initialState = {
+//     todos: [],
+//     open: false,
+//     inputValue: "",
+//     descriptionValue: "",
+//     selectedTag: "",
+//     filterTag: "",
+//     editingIndex: null,
+//     hideDone: false,
+//   };
+//   const todosReducer = (state = initialState, action) => {
+//     switch (action.type) {
+//       case 'SET_OPEN':
+//         return { ...state, open: action.payload };
+//       case 'SET_TODOS':
+//         return { ...state, todos: action.payload };
+//       case 'SET_INPUT_VALUE':
+//         return { ...state, inputValue: action.payload };
+//       case 'SET_DESCRIPTION_VALUE':
+//         return { ...state, descriptionValue: action.payload };
+//       case 'SET_SELECTED_TAG':
+//         return { ...state, selectedTag: action.payload };
+//       case 'SET_FILTER_TAG':
+//         return { ...state, filterTag: action.payload };
+//       case 'SET_EDITING_INDEX':
+//         return { ...state, editingIndex: action.payload };
+//       case 'SET_HIDE_DONE':
+//         return { ...state, hideDone: !state.hideDone }; 
+//       default:
+//         return state;
+//     }
+//   };
+//   export default todosReducer;
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
+const loadTodos = ()=>{
+    try {
+        const serializedTodos = localStorage.getItem("todos");
+        if (serializedTodos === null) return [];
+        return JSON.parse(serializedTodos);
+    } catch (err) {
+        console.error("Could not load todos from localStorage", err);
+        return [];
+    }
+};
+const saveTodos = (todos)=>{
+    try {
+        const serializedTodos = JSON.stringify(todos);
+        localStorage.setItem("todos", serializedTodos);
+    } catch (err) {
+        console.error("Could not save todos to localStorage", err);
+    }
+};
 const initialState = {
-    todos: [],
+    todos: loadTodos(),
     open: false,
     inputValue: "",
     descriptionValue: "",
@@ -41986,10 +42037,12 @@ const todosReducer = (state = initialState, action)=>{
                 open: action.payload
             };
         case "SET_TODOS":
-            return {
+            const updatedState = {
                 ...state,
                 todos: action.payload
             };
+            saveTodos(updatedState.todos);
+            return updatedState;
         case "SET_INPUT_VALUE":
             return {
                 ...state,
--- src/redux/todosSlice.js
+++ src/redux/todosSlice.js
@@ -1,36 +1,97 @@
-const initialState = {
-    todos: [],
-    open: false,
-    inputValue: "",
-    descriptionValue: "",
-    selectedTag: "",
-    filterTag: "",
-    editingIndex: null,
-    hideDone: false,
-  };
+// const initialState = {
+//     todos: [],
+//     open: false,
+//     inputValue: "",
+//     descriptionValue: "",
+//     selectedTag: "",
+//     filterTag: "",
+//     editingIndex: null,
+//     hideDone: false,
+//   };
   
-  const todosReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case 'SET_OPEN':
-        return { ...state, open: action.payload };
-      case 'SET_TODOS':
-        return { ...state, todos: action.payload };
-      case 'SET_INPUT_VALUE':
-        return { ...state, inputValue: action.payload };
-      case 'SET_DESCRIPTION_VALUE':
-        return { ...state, descriptionValue: action.payload };
-      case 'SET_SELECTED_TAG':
-        return { ...state, selectedTag: action.payload };
-      case 'SET_FILTER_TAG':
-        return { ...state, filterTag: action.payload };
-      case 'SET_EDITING_INDEX':
-        return { ...state, editingIndex: action.payload };
-      case 'SET_HIDE_DONE':
-        return { ...state, hideDone: !state.hideDone }; 
-      default:
-        return state;
+//   const todosReducer = (state = initialState, action) => {
+//     switch (action.type) {
+//       case 'SET_OPEN':
+//         return { ...state, open: action.payload };
+//       case 'SET_TODOS':
+//         return { ...state, todos: action.payload };
+//       case 'SET_INPUT_VALUE':
+//         return { ...state, inputValue: action.payload };
+//       case 'SET_DESCRIPTION_VALUE':
+//         return { ...state, descriptionValue: action.payload };
+//       case 'SET_SELECTED_TAG':
+//         return { ...state, selectedTag: action.payload };
+//       case 'SET_FILTER_TAG':
+//         return { ...state, filterTag: action.payload };
+//       case 'SET_EDITING_INDEX':
+//         return { ...state, editingIndex: action.payload };
+//       case 'SET_HIDE_DONE':
+//         return { ...state, hideDone: !state.hideDone }; 
+//       default:
+//         return state;
+//     }
+//   };
+  
+//   export default todosReducer;
+  
+
+
+const loadTodos = () => {
+  try {
+    const serializedTodos = localStorage.getItem("todos");
+    if (serializedTodos === null) {
+      return [];
     }
-  };
-  
-  export default todosReducer;
-  
+    return JSON.parse(serializedTodos);
+  } catch (err) {
+    console.error("Could not load todos from localStorage", err);
+    return [];
+  }
+};
+
+const saveTodos = (todos) => {
+  try {
+    const serializedTodos = JSON.stringify(todos);
+    localStorage.setItem("todos", serializedTodos);
+  } catch (err) {
+    console.error("Could not save todos to localStorage", err);
+  }
+};
+
+const initialState = {
+  todos: loadTodos(),
+  open: false,
+  inputValue: "",
+  descriptionValue: "",
+  selectedTag: "",
+  filterTag: "",
+  editingIndex: null,
+  hideDone: false,
+};
+
+const todosReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'SET_OPEN':
+      return { ...state, open: action.payload };
+    case 'SET_TODOS':
+      const updatedState = { ...state, todos: action.payload };
+      saveTodos(updatedState.todos);
+      return updatedState;
+    case 'SET_INPUT_VALUE':
+      return { ...state, inputValue: action.payload };
+    case 'SET_DESCRIPTION_VALUE':
+      return { ...state, descriptionValue: action.payload };
+    case 'SET_SELECTED_TAG':
+      return { ...state, selectedTag: action.payload };
+    case 'SET_FILTER_TAG':
+      return { ...state, filterTag: action.payload };
+    case 'SET_EDITING_INDEX':
+      return { ...state, editingIndex: action.payload };
+    case 'SET_HIDE_DONE':
+      return { ...state, hideDone: !state.hideDone };
+    default:
+      return state;
+  }
+};
+
+export default todosReducer;
